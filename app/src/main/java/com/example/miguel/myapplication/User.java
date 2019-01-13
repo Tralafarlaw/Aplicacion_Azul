75d0472b2554a612f6fe3c88a44d772827e5c750
TheProdigyK <luisale0410@gmail.com>
2019-01-13 17:38:06 -0400

    User.java

---
+++ app/src/main/java/com/example/miguel/myapplication/User.java
@@ -0,0 +1,67 @@
+package com.example.miguel.myapplication;
+
+public class User {
+    public String Dueño;
+    public Double lat;
+    public Double lon;
+    public String Nombre;
+    public String Placa;
+    public int Status;
+
+    public User(String dueño, Double lat, Double lon, String nombre, String placa, int status) {
+        Dueño = dueño;
+        this.lat = lat;
+        this.lon = lon;
+        Nombre = nombre;
+        Placa = placa;
+        Status = status;
+    }
+
+    public String getDueño() {
+        return Dueño;
+    }
+
+    public void setDueño(String dueño) {
+        Dueño = dueño;
+    }
+
+    public Double getLat() {
+        return lat;
+    }
+
+    public void setLat(Double lat) {
+        this.lat = lat;
+    }
+
+    public Double getLon() {
+        return lon;
+    }
+
+    public void setLon(Double lon) {
+        this.lon = lon;
+    }
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String nombre) {
+        Nombre = nombre;
+    }
+
+    public String getPlaca() {
+        return Placa;
+    }
+
+    public void setPlaca(String placa) {
+        Placa = placa;
+    }
+
+    public int getStatus() {
+        return Status;
+    }
+
+    public void setStatus(int status) {
+        Status = status;
+    }
+}
