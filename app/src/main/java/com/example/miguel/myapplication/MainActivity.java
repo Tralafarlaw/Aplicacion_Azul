ad475e61d5b30312a3ad5f9effbfca96a605cef3
TheProdigyK <luisale0410@gmail.com>
2019-01-27 20:00:09 -0400

    Permisos write and read

--- app/src/main/java/com/example/miguel/myapplication/MainActivity.java
+++ app/src/main/java/com/example/miguel/myapplication/MainActivity.java
@@ -5,12 +5,14 @@ import android.content.Intent;
 import android.content.IntentSender;
 import android.content.pm.PackageManager;
 import android.location.Location;
+import android.os.Build;
 import android.support.annotation.NonNull;
 import android.support.design.widget.TextInputEditText;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -45,6 +47,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private final static int REQUEST_CHECK_SETTINGS_GPS=0x1;
     private final static int REQUEST_ID_MULTIPLE_PERMISSIONS=0x2;
 
+    //write and read
+    private static final String TAG = "MainActivity";
+    private static final int REQUEST_CODE = 1;
+
     @Override
     protected void onStart() {
         super.onStart();
@@ -62,6 +68,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         setUpGClient();
+        setUpWR();
         init();
 
     }
@@ -123,6 +130,26 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
 
     }
+
+    //Permisos write and read
+    private void setUpWR() {
+
+            Log.d(TAG, "Verificando permisos :u");
+            String[] permissions = {Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE};
+            if(ContextCompat.checkSelfPermission(this.getApplicationContext(),permissions[0]) == PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(this.getApplicationContext(), permissions[1]) == PackageManager.PERMISSION_GRANTED)
+            {
+
+            }
+            else
+            {
+                ActivityCompat.requestPermissions(MainActivity.this,permissions,REQUEST_CODE);
+            }
+
+
+
+    }
+
+
 
     //Permisos de google play
     private synchronized void setUpGClient() {
@@ -251,5 +278,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         if (permissionLocation == PackageManager.PERMISSION_GRANTED) {
             getMyLocation();
         }
+        setUpWR();
     }
 }
--- app/src/main/java/com/example/miguel/myapplication/TrackActivity.java
+++ app/src/main/java/com/example/miguel/myapplication/TrackActivity.java
@@ -229,8 +229,22 @@ public class TrackActivity extends AppCompatActivity implements LocationListener
     @Override
     public void onStatusChanged(String provider, int status, Bundle extras) {
         //Cambio el estado o Proveedor del GPS la verdad no entiendo bien que hace
-        txt4.setText("Estado: ERRROR GPS");
-        estado.setImageResource(R.drawable.naranjaalert);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        else
+        {
+            txt4.setText("Estado: ERRROR GPS");
+            estado.setImageResource(R.drawable.naranjaalert);
+        }
+
 
     }
 
